d29d8413351e5ba684e52591f2f2ffc92477769b
EricTheRay <121706137+EricTheRay@users.noreply.github.com>
2024-02-06 11:10:26 +0800

    Made changes to have apps on subdomains.

--- word_guesser_django/word_guesser_django/urls.py
+++ word_guesser_django/word_guesser_django/urls.py
@@ -20,6 +20,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('dictionary.urls'))
+    path('word-guesser/admin/', admin.site.urls),
+    path('word-guesser/api/', include('dictionary.urls'))
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
