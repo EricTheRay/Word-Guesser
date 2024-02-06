0d50e9cc25ddd6c3c596acd04ab749f95119d11d
EricTheRay <121706137+EricTheRay@users.noreply.github.com>
2024-02-06 11:18:34 +0800

    Made message bubbles transparent to mouse clicks.

--- word_guesser_vue/src/App.vue
+++ word_guesser_vue/src/App.vue
@@ -319,7 +319,7 @@ onBeforeUnload(() => {
       </div>
     </div>
 
-    <div class="fixed top-[25%] w-full z-20">
+    <div class="fixed top-[25%] w-full z-20 pointer-events-none">
       <MessageBubbles 
         id="bubbles" 
         v-bind:store-id="'bubbles-0'"
