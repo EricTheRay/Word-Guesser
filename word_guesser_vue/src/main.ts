94003cb8e81ec7017cd381e6ce5c253fd21d8a03
EricTheRay <121706137+EricTheRay@users.noreply.github.com>
2024-04-06 19:45:05 +0800

    Fixed environment variables to match development and production settings.

--- word_guesser_vue/src/main.ts
+++ word_guesser_vue/src/main.ts
@@ -7,7 +7,7 @@ import App from './App.vue';
 import router from './router';
 import axios from 'axios';
 
-axios.defaults.baseURL = import.meta.env.AXIOS_BASE_URL;
+axios.defaults.baseURL = import.meta.env.VITE_AXIOS_BASE_URL;
 
 const app = createApp(App);
 
